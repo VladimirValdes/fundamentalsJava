df7c77c38dc135bc58198e21fb764301bd75511d
Vladimir <mendozahum20@gmail.com>
2022-06-25 19:52:25 -0600

    feat: add Invoice class

---
+++ HelloWorld/src/poo/InvoiceItem.java
@@ -0,0 +1,51 @@
+package poo;
+
+public class InvoiceItem {
+	
+	String id;
+	String desc;
+	int qty;
+	double unitPrice;
+	
+	public InvoiceItem( String id, String desc, int qty, double unitPrice) {
+		this.id = id;
+		this.desc = desc;
+		this.qty = qty;
+		this.unitPrice = unitPrice;
+	}
+	
+	public String getID() {
+		return this.id;
+	}
+	
+	public String getDesc() {
+		return this.desc;
+	}
+	
+	public int Qty() {
+		return this.qty;
+	}
+	
+	public double getUnitPrice() {
+		return this.unitPrice;
+	}
+	
+	public void setQty( int qty ) {
+		this.qty = qty;
+	}
+	
+	public void setUnitPrice( double unitPrice ) {
+		this.unitPrice = unitPrice;
+	}
+	
+	public double getTotal() {
+		return this.unitPrice * this.qty;
+	}
+	
+	public String toString() {
+		return "InvoiceItem[ id = " + this.id + " DESC = " + this.desc + " " + " QTY = " +
+				this.qty + " " + " unitPrice " + this.unitPrice + " ]";
+	}
+	
+
+}
